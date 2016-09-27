ad6e316aaf177bd805834dfc7ab840db893e7d5f
Konrad Pason <konrad.pason17@gmail.com>
2016-09-27 11:21:11 +0200

    Changed styling

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,5 +1,31 @@
 class DishesController < ApplicationController
 
+
 	def index
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def dish_params
+		params.require(:dish).permit(:name, :description, :category_id)
+	end
+
+	def find_dish
+		@dish = Dish.find(params[:id])
 	end
 end
